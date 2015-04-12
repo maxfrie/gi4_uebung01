3751911efee12dbc1f15a7cb3f194ad3f2e1b384
maxfrie <kontakt@maxfriedel.de>
2015-04-12 14:45:15 +0200

    Programm myecho hinzugefuegt

--- myecho.c
+++ myecho.c
@@ -2,5 +2,12 @@
 
 int main(int argc, char* argv[], char* envp[]) 
 {
+  int i;
+  for(i = 1; i < argc; i++)
+  {
+	printf(argv[i]);
+	if(i + 1 == argc) printf("\n");
+	else printf(" ");
+  }
   return 0;
 }
