386034ac57d5c122b18c3640b056952d5ad52570
maxfrie <kontakt@maxfriedel.de>
2015-04-13 15:48:34 +0200

    modified myexec.c with fork

--- myexec.c
+++ myexec.c
@@ -3,7 +3,13 @@
 
 int main(int argc, char* argv[], char* envp[]) 
 {
-  execv(argv[1], argv+1);
+  pid_t pid = fork();
+  if(pid > 0) {
+	printf("Prozess ID - parent: %d\n", getpid());
+	printf("Prozess ID - child: %d\n", pid);
+  }
+  if(pid == 0)
+	execv(argv[1], argv+1);
 
   return 0;
 }
